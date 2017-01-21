dbe0cdad4fcb75a1297f03829d5d6501bcadd726
DESKTOP-DSFK7RL\Martin <martin.penard@gmail.com>
2017-01-21 18:26:30 +0100

    + assets

--- Assets/_assets/2.Scripts/SandGrid.cs
+++ Assets/_assets/2.Scripts/SandGrid.cs
@@ -16,14 +16,11 @@ public class SandGrid : MonoBehaviour
     {
         tiles = new SandTile[x,y];
         tileSize = tilePrefab.GetComponent<SandTile>().Size;
-        Debug.Log("tilesize:" + tileSize);
-        //tilePrefab.GetComponent<SandTile>().Size = tileSize;
         for (int i = 0; i < x; i++)
         {
             for (int j = 0; j < y; j++)
             {
                 Vector3 position = new Vector3(topLeftCornerPosition.position.x + tileSize.x * i, topLeftCornerPosition.position.y, topLeftCornerPosition.position.z - tileSize.x * j);
-                //Debug.Log(position);
                 GameObject tileObj = (GameObject)Instantiate(tilePrefab, position, new Quaternion());
                 
                 tiles[i,j] = tileObj.GetComponent<SandTile>();
