96e6313afd46aa7b12369891176a4bc02fd066e6
DESKTOP-DSFK7RL\Martin <martin.penard@gmail.com>
2017-01-22 09:47:37 +0100

    intégration UI en cours

---
+++ Assets/_assets/2.Scripts/ButtonsHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonsHandler : MonoBehaviour
+{
+
+
+    public GameObject bucketBase;
+    public GameObject bucket1;
+    public GameObject bucket2;
+    public GameObject bucket3;
+
+    public GameObject rockBase;
+    public GameObject rock1;
+    public GameObject rock2;
+    public GameObject rock3;
+
+    public GameObject seaweedBase;
+    public GameObject seaweed1;
+    public GameObject seaweed2;
+    public GameObject seaweed3;
+
+
+
+    private int rockValue;
+    private int seaweedValue;
+    private int bucketValue;
+
+    public void ChangeRockAmount(int amount)
+    {
+
+    }
+
+    public void ChangeSeaAmount(int amount)
+    {
+
+    }
+
+    public void ChangeBucketAmount(int amount)
+    {
+
+    }
+
+
+}
