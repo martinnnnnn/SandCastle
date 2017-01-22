24ad4ebfc0980b660dc65d5c2f3ef07a2de5db82
tabasscore <antonin.demeilliez@gmail.com>
2017-01-22 06:22:25 +0100

    UIantonin

---
+++ Assets/NGUI/Scripts/UI/MakeItGlow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeItGlow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+	}
+	
+    
+	// Update is called once per frame
+   
+
+	public void Glow () {
+
+
+        if (gameObject.tag == ("shovelbutton"))
+        {
+            if (gameObject.activeInHierarchy)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+            }
+        }
+
+        if (gameObject.tag == ("rocksbutton"))
+        {
+            if (gameObject.activeInHierarchy)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+            }
+        }
+        if (gameObject.tag == ("seaweedbutton"))
+        {
+           
+            if (gameObject.activeInHierarchy)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+            }
+
+        }
+
+      
+
+
+    }
+}
